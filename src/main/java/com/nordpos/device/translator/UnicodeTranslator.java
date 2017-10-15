5233a81504262085b0e3a1148ba2df344916a397
Jan Otto <jan.otto96@gmail.com>
2017-10-15 16:10:35 +0200

    changed unicode translator

--- src/main/java/com/nordpos/device/translator/UnicodeTranslator.java
+++ src/main/java/com/nordpos/device/translator/UnicodeTranslator.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along with
  * NORD POS. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.nordpos.device.traslator;
+package com.nordpos.device.translator;
 
 /**
  *
